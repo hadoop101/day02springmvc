a965e83980d63655dd0d1c0b3f70dd8d384d4523
hadoop101 <hadoop101@126.com>
2020-06-21 11:43:29 +0800

    页面跳转，使用String也可以使用ModelAndView

--- m2_springmvc/src/main/java/pack01/controller/LoginController.java
+++ m2_springmvc/src/main/java/pack01/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import pack01.bean.Person;
 import pack01.service.PersonSrevice;
 import pack01.service.impl.PersonSreviceImpl;
@@ -13,6 +14,7 @@ import pack01.service.impl.PersonSreviceImpl;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Controller
 @RequestMapping("/person")
@@ -42,6 +44,7 @@ public class LoginController {
     }
 
      */
+    /*
     @RequestMapping(path = "/login",method = {RequestMethod.GET,RequestMethod.POST})
     public String login(Person person) {
         //1:获取页面的账号与密码
@@ -51,6 +54,45 @@ public class LoginController {
         int result = personSrevice.login(person.getName(),person.getPassword());
         //3:返回结果int  1,-1 账号不存在，-2密码出错
         return result == 1 ? "success":"failure";
+    }*/
+
+    /*
+    @RequestMapping(path = "/login",method = {RequestMethod.GET,RequestMethod.POST})
+    public String login(Person person,HttpServletRequest request) {
+        //1:获取页面的账号与密码
+        //2:业务查询账户密码是否正确
+        System.out.println(person);
+        PersonSrevice personSrevice=new PersonSreviceImpl();
+        int result = personSrevice.login(person.getName(),person.getPassword());
+        if(result == 1){
+            //查所有用户数据带到页面去显示list.jsp
+            List<Person> list = personSrevice.searchPerson("AAA");
+            request.setAttribute("list",list);
+            return  "list";//默认是forward
+        }else{
+            
+            return "failure";
+        }
+    }
+    */
+    @RequestMapping(path = "/login",method = {RequestMethod.GET,RequestMethod.POST})
+    public ModelAndView login(Person person) {
+        //1:获取页面的账号与密码
+        //2:业务查询账户密码是否正确
+        System.out.println(person);
+        PersonSrevice personSrevice=new PersonSreviceImpl();
+        int result = personSrevice.login(person.getName(),person.getPassword());
+        ModelAndView mv  = new ModelAndView();
+        if(result == 1){
+            //查所有用户数据带到页面去显示list.jsp
+            List<Person> list = personSrevice.searchPerson("AAA");
+            mv.addObject("list",list);//添加一个键值对，参1 键 参2 值
+            mv.setViewName("list"); //默认是forward
+        }else{
+
+            mv.setViewName("failure");
+        }
+        return mv;
     }
 
 
---
+++ m2_springmvc/src/main/webapp/WEB-INF/views/list.jsp
@@ -0,0 +1,7 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+<html>
+<body>
+<h2>所有用户</h2>
+${list}
+</body>
+</html>
