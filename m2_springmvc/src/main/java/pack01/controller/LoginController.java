be7018e5cc87b744f01df74214f06983fe1a9f77
hadoop101 <hadoop101@126.com>
2020-06-21 11:09:37 +0800

    登录功能的登录页面 参数使用request获取

--- m2_springmvc/src/main/java/pack01/controller/LoginController.java
+++ m2_springmvc/src/main/java/pack01/controller/LoginController.java
@@ -1,16 +1,22 @@
 package pack01.controller;
 
 
+import com.sun.deploy.net.HttpResponse;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import pack01.service.PersonSrevice;
 import pack01.service.impl.PersonSreviceImpl;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 @RequestMapping("/person")
 public class LoginController {
-
+    /*
     @RequestMapping(path = "/login",method = {RequestMethod.GET,RequestMethod.POST})
     public String login(String username,String password) {
         //1:获取页面的账号与密码
@@ -20,4 +26,19 @@ public class LoginController {
         //3:返回结果int  1,-1 账号不存在，-2密码出错
         return result == 1 ? "success":"failure";
     }
+    */
+
+    @RequestMapping(path = "/login",method = {RequestMethod.GET,RequestMethod.POST})
+    public String login(HttpServletRequest request, HttpServletResponse response) {
+        //1:获取页面的账号与密码
+        //2:业务查询账户密码是否正确
+        String username=request.getParameter("username");
+        String password=request.getParameter("password");
+        PersonSrevice personSrevice=new PersonSreviceImpl();
+        int result = personSrevice.login(username,password);
+        //3:返回结果int  1,-1 账号不存在，-2密码出错
+        return result == 1 ? "success":"failure";
+    }
+
+
 }
