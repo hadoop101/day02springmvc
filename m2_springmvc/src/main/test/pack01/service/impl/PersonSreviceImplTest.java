5aed88e762ce0568f5fbb5e604499a2d4921aa90
hadoop101 <hadoop101@126.com>
2020-06-21 11:03:53 +0800

    登录功能的控制器Controller

--- m2_springmvc/src/main/test/pack01/service/impl/PersonSreviceImplTest.java
+++ m2_springmvc/src/main/test/pack01/service/impl/PersonSreviceImplTest.java
@@ -14,7 +14,7 @@ public class PersonSreviceImplTest {
         String password="1234";
         //2:业务查询账户密码是否正确
         PersonSrevice personSrevice=new PersonSreviceImpl();
-       int result = personSrevice.login(username,password);
+        int result = personSrevice.login(username,password);
         //3:返回结果int  1,-1 账号不存在，-2密码出错
         System.out.println(result);
     }
