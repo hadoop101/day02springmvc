1da0d8d8ee453f34e860a27192e1e084f4742a59
hadoop101 <hadoop101@126.com>
2020-06-21 11:14:14 +0800

    登录功能的登录页面 login3.jsp页面 请求参数名=对象的成员变量名

--- m2_springmvc/src/main/java/pack01/controller/LoginController.java
+++ m2_springmvc/src/main/java/pack01/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import pack01.bean.Person;
 import pack01.service.PersonSrevice;
 import pack01.service.impl.PersonSreviceImpl;
 
@@ -27,7 +28,7 @@ public class LoginController {
         return result == 1 ? "success":"failure";
     }
     */
-
+    /*
     @RequestMapping(path = "/login",method = {RequestMethod.GET,RequestMethod.POST})
     public String login(HttpServletRequest request, HttpServletResponse response) {
         //1:获取页面的账号与密码
@@ -36,6 +37,18 @@ public class LoginController {
         String password=request.getParameter("password");
         PersonSrevice personSrevice=new PersonSreviceImpl();
         int result = personSrevice.login(username,password);
+        //3:返回结果int  1,-1 账号不存在，-2密码出错
+        return result == 1 ? "success":"failure";
+    }
+
+     */
+    @RequestMapping(path = "/login",method = {RequestMethod.GET,RequestMethod.POST})
+    public String login(Person person) {
+        //1:获取页面的账号与密码
+        //2:业务查询账户密码是否正确
+        System.out.println(person);
+        PersonSrevice personSrevice=new PersonSreviceImpl();
+        int result = personSrevice.login(person.getName(),person.getPassword());
         //3:返回结果int  1,-1 账号不存在，-2密码出错
         return result == 1 ? "success":"failure";
     }
---
+++ m2_springmvc/src/main/webapp/login3.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2020/6/21
+  Time: 10:35
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>登录页面</title>
+</head>
+<body>
+    <form action="/m2_springmvc/person/login" method="post">
+        <input type="text" name="name">
+        <input type="password" name="password">
+        <input type="submit" value="登录">
+    </form>
+</body>
+</html>
